0c19291fb1aebeeda53c5d4eb04ad3bbfc31760d
ess <ess@recordedfuture.com>
2019-04-25 08:54:19 +0000

    Added some copyright notices.
    git-svn-id: https://svn.recfut.com/branches/dev-harvest-async@132988 b1fed77e-75fb-430b-af2f-0c3e521289a6

--- v2.0/recordedfuture/recordedfuture_connector.py
+++ v2.0/recordedfuture/recordedfuture_connector.py
@@ -1,3 +1,15 @@
+#
+# File: recordedfuture_connector.py
+#
+# Copyright (c) Recorded Future, Inc., 2019
+#
+# This unpublished material is proprietary to Recorded Future.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Recorded Future.
+#
 # ---------------------------------------------
 # Phantom Recorded Future Connector python file
 # ---------------------------------------------
--- v2.0/recordedfuture/recordedfuture_consts.py
+++ v2.0/recordedfuture/recordedfuture_consts.py
@@ -1,3 +1,18 @@
+#
+# File: recordedfuture_consts.py
+#
+# Copyright (c) Recorded Future, Inc., 2019
+#
+# This unpublished material is proprietary to Recorded Future.
+# All rights reserved. The methods and
+# techniques described herein are considered trade secrets
+# and/or confidential. Reproduction or distribution, in whole
+# or in part, is forbidden except by express written permission
+# of Recorded Future.
+#
+# ---------------------------------------------
+# Phantom Recorded Future Connector python file
+# ---------------------------------------------
 
 # Define your constants here
 version = '%RELEASE%'
