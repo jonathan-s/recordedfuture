2bded46363d289be00bd0d311805f5bcefa38e89
ess <ess@recordedfuture.com>
2019-05-13 13:15:07 +0000

    Added a cs1Label requirement for the vulnerability branch of the and the corresponding artifact.
    git-svn-id: https://svn.recfut.com/branches/dev-harvest-async@134212 b1fed77e-75fb-430b-af2f-0c3e521289a6

---
+++ v2.0/test/playbooks/recorded_future_reputation_test/recorded_future_reputation_test.py
@@ -0,0 +1,201 @@
+"""
+"""
+
+import phantom.rules as phantom
+import json
+from datetime import datetime, timedelta
+
+def on_start(container):
+    phantom.debug('on_start() called')
+    
+    # call 'decision_1' block
+    decision_1(container=container)
+
+    return
+
+def ip_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('ip_reputation_1() called')
+
+    # collect data for 'ip_reputation_1' call
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.destinationAddress', 'artifact:*.id'])
+
+    parameters = []
+    
+    # build parameters list for 'ip_reputation_1' call
+    for container_item in container_data:
+        if container_item[0]:
+            parameters.append({
+                'ip': container_item[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': container_item[1]},
+            })
+
+    phantom.act("ip reputation", parameters=parameters, assets=['recordedfuture'], name="ip_reputation_1")
+
+    return
+
+def decision_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('decision_1() called')
+
+    # check for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.fileHash", "!=", ""],
+        ])
+
+    # call connected blocks if condition 1 matched
+    if matched_artifacts_1 or matched_results_1:
+        file_reputation_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'elif' condition 2
+    matched_artifacts_2, matched_results_2 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.cs1", "!=", ""],
+            ["artifact:*.cef.cs1Label", "==", "vulnerability"],
+        ],
+        logical_operator='and')
+
+    # call connected blocks if condition 2 matched
+    if matched_artifacts_2 or matched_results_2:
+        lookup_vulnerability_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'elif' condition 3
+    matched_artifacts_3, matched_results_3 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.destinationAddress", "!=", ""],
+        ])
+
+    # call connected blocks if condition 3 matched
+    if matched_artifacts_3 or matched_results_3:
+        ip_reputation_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'elif' condition 4
+    matched_artifacts_4, matched_results_4 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.destinationDnsDomain", "!=", ""],
+        ])
+
+    # call connected blocks if condition 4 matched
+    if matched_artifacts_4 or matched_results_4:
+        domain_reputation_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    # check for 'elif' condition 5
+    matched_artifacts_5, matched_results_5 = phantom.condition(
+        container=container,
+        conditions=[
+            ["artifact:*.cef.requestURL", "!=", ""],
+        ])
+
+    # call connected blocks if condition 5 matched
+    if matched_artifacts_5 or matched_results_5:
+        url_reputation_1(action=action, success=success, container=container, results=results, handle=handle)
+        return
+
+    return
+
+def domain_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('domain_reputation_1() called')
+
+    # collect data for 'domain_reputation_1' call
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.destinationDnsDomain', 'artifact:*.id'])
+
+    parameters = []
+    
+    # build parameters list for 'domain_reputation_1' call
+    for container_item in container_data:
+        if container_item[0]:
+            parameters.append({
+                'domain': container_item[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': container_item[1]},
+            })
+
+    phantom.act("domain reputation", parameters=parameters, assets=['recordedfuture'], name="domain_reputation_1")
+
+    return
+
+def file_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('file_reputation_1() called')
+
+    # collect data for 'file_reputation_1' call
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.fileHash', 'artifact:*.id'])
+
+    parameters = []
+    
+    # build parameters list for 'file_reputation_1' call
+    for container_item in container_data:
+        if container_item[0]:
+            parameters.append({
+                'hash': container_item[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': container_item[1]},
+            })
+
+    phantom.act("file reputation", parameters=parameters, assets=['recordedfuture'], name="file_reputation_1")
+
+    return
+
+def lookup_vulnerability_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('lookup_vulnerability_1() called')
+
+    # collect data for 'lookup_vulnerability_1' call
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.cs1', 'artifact:*.id'])
+
+    parameters = []
+    
+    # build parameters list for 'lookup_vulnerability_1' call
+    for container_item in container_data:
+        if container_item[0]:
+            parameters.append({
+                'vulnerability': container_item[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': container_item[1]},
+            })
+
+    phantom.act("lookup vulnerability", parameters=parameters, assets=['recordedfuture'], name="lookup_vulnerability_1")
+
+    return
+
+def url_reputation_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('url_reputation_1() called')
+
+    # collect data for 'url_reputation_1' call
+    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.requestURL', 'artifact:*.id'])
+
+    parameters = []
+    
+    # build parameters list for 'url_reputation_1' call
+    for container_item in container_data:
+        if container_item[0]:
+            parameters.append({
+                'url': container_item[0],
+                # context (artifact id) is added to associate results with the artifact
+                'context': {'artifact_id': container_item[1]},
+            })
+
+    phantom.act("url reputation", parameters=parameters, assets=['recordedfuture'], name="url_reputation_1")
+
+    return
+
+def on_finish(container, summary):
+    phantom.debug('on_finish() called')
+    # This function is called after all actions are completed.
+    # summary of all the action and/or all detals of actions 
+    # can be collected here.
+
+    # summary_json = phantom.get_summary()
+    # if 'result' in summary_json:
+        # for action_result in summary_json['result']:
+            # if 'action_run_id' in action_result:
+                # action_results = phantom.get_action_results(action_run_id=action_result['action_run_id'], result_data=False, flatten=False)
+                # phantom.debug(action_results)
+
+    return
