4ed0752b9a690a99d0c676a360f0a7c390a2dd13
ess <ess@recordedfuture.com>
2019-05-27 10:54:43 +0000

    Removed testing of chained freetext -> rule id -> alerts.
    git-svn-id: https://svn.recfut.com/branches/dev-harvest-async@135027 b1fed77e-75fb-430b-af2f-0c3e521289a6

--- v2.0/test/playbooks/recorded_future_alert_test/recorded_future_alert_test.py
+++ v2.0/test/playbooks/recorded_future_alert_test/recorded_future_alert_test.py
@@ -38,10 +38,8 @@ def decision_1(action=None, success=None, container=None, results=None, handle=N
 
     # call connected blocks if condition 2 matched
     if matched_artifacts_2 or matched_results_2:
-        alert_rule_id_lookup_2(action=action, success=success, container=container, results=results, handle=handle)
+        alert_rule_lookup_1(action=action, success=success, container=container, results=results, handle=handle)
         return
-
-    # call connected blocks for 'else' condition 3
 
     return
 
@@ -92,15 +90,15 @@ def add_comment_1(action=None, success=None, container=None, results=None, handl
 
     return
 
-def alert_rule_id_lookup_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('alert_rule_id_lookup_2() called')
+def alert_rule_lookup_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('alert_rule_lookup_1() called')
 
-    # collect data for 'alert_rule_id_lookup_2' call
+    # collect data for 'alert_rule_lookup_1' call
     container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.cs1', 'artifact:*.id'])
 
     parameters = []
     
-    # build parameters list for 'alert_rule_id_lookup_2' call
+    # build parameters list for 'alert_rule_lookup_1' call
     for container_item in container_data:
         if container_item[0]:
             parameters.append({
@@ -109,7 +107,7 @@ def alert_rule_id_lookup_2(action=None, success=None, container=None, results=No
                 'context': {'artifact_id': container_item[1]},
             })
 
-    phantom.act("alert rule id lookup", parameters=parameters, assets=['recordedfuture'], callback=format_2, name="alert_rule_id_lookup_2")
+    phantom.act("alert rule lookup", parameters=parameters, assets=['recordedfuture'], callback=format_2, name="alert_rule_lookup_1")
 
     return
 
@@ -120,7 +118,7 @@ def format_2(action=None, success=None, container=None, results=None, handle=Non
 
     # parameter list for template variable replacement
     parameters = [
-        "alert_rule_id_lookup_2:action_result.message",
+        "alert_rule_lookup_1:action_result.summary.rule_id_list",
     ]
 
     phantom.format(container=container, template=template, parameters=parameters, name="format_2")
@@ -133,58 +131,6 @@ def add_comment_2(action=None, success=None, container=None, results=None, handl
     phantom.debug('add_comment_2() called')
 
     formatted_data_1 = phantom.get_format_data(name='format_2')
-
-    phantom.comment(container=container, comment=formatted_data_1)
-    alert_data_lookup_2(container=container)
-
-    return
-
-def alert_data_lookup_2(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('alert_data_lookup_2() called')
-    
-    #phantom.debug('Action: {0} {1}'.format(action['name'], ('SUCCEEDED' if success else 'FAILED')))
-    
-    # collect data for 'alert_data_lookup_2' call
-    container_data = phantom.collect2(container=container, datapath=['artifact:*.cef.cs2', 'artifact:*.id'])
-    results_data_1 = phantom.collect2(container=container, datapath=['alert_rule_id_lookup_2:action_result.data.*.rule.id', 'alert_rule_id_lookup_2:action_result.parameter.context.artifact_id'], action_results=results)
-
-    parameters = []
-    
-    # build parameters list for 'alert_data_lookup_2' call
-    for container_item in container_data:
-        for results_item_1 in results_data_1:
-            if results_item_1[0] and container_item[0]:
-                parameters.append({
-                    'rule_id': results_item_1[0],
-                    'timeframe': container_item[0],
-                    # context (artifact id) is added to associate results with the artifact
-                    'context': {'artifact_id': results_item_1[1]},
-                })
-
-    phantom.act("alert data lookup", parameters=parameters, assets=['recordedfuture'], callback=format_3, name="alert_data_lookup_2")
-
-    return
-
-def format_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('format_3() called')
-    
-    template = """{0}"""
-
-    # parameter list for template variable replacement
-    parameters = [
-        "alert_data_lookup_2:action_result.message",
-    ]
-
-    phantom.format(container=container, template=template, parameters=parameters, name="format_3")
-
-    add_comment_3(container=container)
-
-    return
-
-def add_comment_3(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
-    phantom.debug('add_comment_3() called')
-
-    formatted_data_1 = phantom.get_format_data(name='format_3')
 
     phantom.comment(container=container, comment=formatted_data_1)
 
