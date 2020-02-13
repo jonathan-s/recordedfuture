9c5a06e364bda7272af8803c60ecc4d27ba10be4
ess <ess@recordedfuture.com>
2020-02-13 11:58:13 +0100

    Adapted to the API deploy with the correct name for c2. Added test that verifies that a few verdicts come out true.

--- src/test/test_action_threat_assessment.py
+++ src/test/test_action_threat_assessment.py
@@ -1,6 +1,7 @@
 """Test suite for domain reputation action"""
 import os
 import logging
+import unittest
 import requests
 from test_harness import RfTests
 import rfapi
@@ -8,10 +9,24 @@ import rfapi
 # disable certificate warnings for self signed certificates
 requests.packages.urllib3.disable_warnings()
 
+# Test these contexts
+CTXLIST = ['c2', 'phishing']
+THRESHOLD = 60
+
 # Logger
 LOGGER = logging.getLogger(__name__)
 
 PBOOK = 'recorded_future_threat_assessment_test'
+TESTDATA = dict(destinationAddress=['8.8.8.8'],
+                destinationDnsDomain=['example.com'],
+                fileHash=['d41d8cd98f00b204e9800998ecf8427e'],  # emtpty file
+                requestURL=['https://www.example.com'])
+DGTOCEF = {
+    'ip': 'destinationAddress',
+    'domain': 'destinationDnsDomain',
+    'hash': 'fileHash',
+    'url': 'requestURL'
+}
 
 
 class RfThreatAssessmentTests(RfTests):
@@ -21,7 +36,8 @@ class RfThreatAssessmentTests(RfTests):
         """Setup test environment."""
         RfTests.setUp(self, PBOOK)
 
-    def test_threat_assessment(self):
+    # @unittest.skip('Lacking testable events for some data groups.')
+    def test_threat_assessment_testability(self):
         """Check that all contexts are testable.
 
         If there are no entities available in any of the used entity types
@@ -32,13 +48,10 @@ class RfThreatAssessmentTests(RfTests):
         conapi = rfapi.ConnectApiClient(auth=os.environ['RF_TOKEN'],
                                         app_name='phantom_unittests')
         ctxlist = conapi._query('soar/triage/contexts')
-        for context in ['c2', 'cnc', 'phishing']:
-            if 'c2' not in ctxlist.result:  # cnc is migrating to c2
-                datagroups = ctxlist.result['cnc']['datagroup'].keys()
-                threshold = ctxlist.result['cnc']['default_threshold']
-            else:
-                datagroups = ctxlist.result[context]['datagroup'].keys()
-                threshold = ctxlist.result[context]['default_threshold']
+        for context in CTXLIST:
+            datagroups = ctxlist.result[context]['datagroup'].keys()
+            # threshold = ctxlist.result[context]['default_threshold']
+            threshold = THRESHOLD
             total_targets = 0
             for datagroup in datagroups:
                 targets = self._get_triage_entities_by_group(
@@ -52,3 +65,89 @@ class RfThreatAssessmentTests(RfTests):
                                 'tested, there are no entities in any group '
                                 'that can produce a positive verdict.'
                                 % context)
+
+    # @unittest.skip('Debug')
+    def test_threat_assessment_positive_verdict(self):
+        """Check that entities with sufficient risk yields positive verdict.
+
+        "Randomly" select at least one entity with sufficient risk for the
+        context and add it along with other entities to an artifact within
+        a container. Run the playbook. Examine result."""
+        conapi = rfapi.ConnectApiClient(auth=os.environ['RF_TOKEN'],
+                                        app_name='phantom_unittests')
+        ctxlist = conapi._query('soar/triage/contexts')
+        print('contexts: %s' % CTXLIST)
+        for context in CTXLIST:
+            datagroups = ctxlist.result[context]['datagroup'].keys()
+            # threshold = ctxlist.result[context]['default_threshold']
+            threshold = THRESHOLD
+            threshold_type = 'max'
+
+            print('datagroups %s: %s' % (context, datagroups))
+            for datagroup in datagroups:
+                targets = self._get_triage_entities_by_group(
+                    datagroup,
+                    '%sSubscore' % context,
+                    5, threshold - 1, 100)
+
+                print('targets %s/%s: %s' % (context, datagroup, targets))
+                if not targets:
+                    self.assertTrue(False, '%s/%s: no targets found.'
+                                    % (context, datagroup))
+                else:
+                    target = targets[0].split(':', 1)[1]
+                    self._test_positive_verdict(context, datagroup,
+                                                target, threshold,
+                                                threshold_type)
+
+    def _create_threat_assessment_event(self,
+                                        context, datagroup, target,
+                                        threshold, threshold_type):
+        """Create a threat assessment event for testing.
+
+        The event is expected to be consumed by the
+        recorded_future_threat_assessment_test playbook."""
+        # Make a copy of the test_data
+        test_data = dict(**TESTDATA)
+        test_data['cs1'] = context
+        test_data['cs1Label'] = 'Threat Assessment Context'
+        test_data['cs2'] = threshold
+        test_data['cs2Label'] = 'Threshold'
+        test_data['cs3'] = threshold_type
+        test_data['cs3Label'] = 'Threshold Type'
+        test_data[DGTOCEF[datagroup]].append(target)
+
+        # Reformat lists into comma separated strings
+        for key in ['ip', 'domain', 'hash', 'url']:
+            test_data[DGTOCEF[key]] = ','.join(
+                test_data[DGTOCEF[key]])
+        print('test_data %s/%s/%s: %s' % (context,
+                                          datagroup,
+                                          target,
+                                          test_data
+                                          ))
+
+        # Create container with artifact
+        container_id = self._create_event_and_artifact(
+            'Test Event Threat Assessment (%s/%s)'
+            % (context, datagroup),
+            **test_data)
+
+        # Wait for finished playbook run and collect result.
+        ares = self._poll_for_success(self._action_result,
+                                      container_id)
+        return ares
+
+    def _test_positive_verdict(self,
+                               context, datagroup, target,
+                               threshold, threshold_type):
+        """Test positive verdict."""
+        # Create event and collect playbook result
+        ares = self._create_threat_assessment_event(context, datagroup,
+                                                    target, threshold,
+                                                    threshold_type)
+        # Verify the verdict
+        verdict = ares['data'][0]['result_data'][0]['data'][0][
+            'verdict']
+        self.assertEqual(verdict, True, '%s/%s did not yield true verdict'
+                         % (context, datagroup))
