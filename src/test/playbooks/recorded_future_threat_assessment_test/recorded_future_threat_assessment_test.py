38b759cafc0973b87ab73595499f1d46176d756c
ess <ess@recordedfuture.com>
2020-02-19 10:36:40 +0100

    Added filter to test playbook to ensure it only acts on the properly tagged test events.

--- src/test/playbooks/recorded_future_threat_assessment_test/recorded_future_threat_assessment_test.py
+++ src/test/playbooks/recorded_future_threat_assessment_test/recorded_future_threat_assessment_test.py
@@ -8,8 +8,8 @@ from datetime import datetime, timedelta
 def on_start(container):
     phantom.debug('on_start() called')
     
-    # call 'threat_assessment_1' block
-    threat_assessment_1(container=container)
+    # call 'filter_1' block
+    filter_1(container=container)
 
     return
 
@@ -36,6 +36,25 @@ def threat_assessment_1(action=None, success=None, container=None, results=None,
         })
 
     phantom.act("threat assessment", parameters=parameters, assets=['recorded-future'], name="threat_assessment_1")
+
+    return
+
+def filter_1(action=None, success=None, container=None, results=None, handle=None, filtered_artifacts=None, filtered_results=None):
+    phantom.debug('filter_1() called')
+    
+    tags_param = container.get('tags', None)
+
+    # collect filtered artifact ids for 'if' condition 1
+    matched_artifacts_1, matched_results_1 = phantom.condition(
+        container=container,
+        conditions=[
+            [tags_param, "==", ['recorded_future_threat_assessment_test']],
+        ],
+        name="filter_1:condition_1")
+
+    # call connected blocks if filtered artifacts or results
+    if matched_artifacts_1 or matched_results_1:
+        threat_assessment_1(action=action, success=success, container=container, results=results, handle=handle, filtered_artifacts=matched_artifacts_1, filtered_results=matched_results_1)
 
     return
 
